a32f508bb7aaefb80a8764f16fb927aeac21379b
Dong Tran <dong.tt@urbox.vn>
2023-12-10 14:34:48 +0700

    using process data from mongodb

--- src/app.js
+++ src/app.js
@@ -2,125 +2,129 @@ const { get } = require("lodash");
 const { v4: uuidv4 } = require("uuid");
 const { Client } = require("pg");
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { parseCurlString } = require("./utils/curl-parser");
 const { performRequest } = require("./utils/axios");
-const { CONFIG } = require("../config");
 const { delayWithAsync } = require("./utils/common");
 const processDataRoutes = require("./routes/process-data");
 const connectToMongo = require("./config/mongo");
+const { ProcessDataModel } = require("./models/process-data");
 const app = express();
 
 async function test() {
   let parameters = {};
   try {
-    parameters = {};
     let index = 0;
-    console.log(`Running: ${CONFIG.name}`);
-    for (const processItem of CONFIG.process) {
-      switch (processItem.name) {
-        case "generate-data": {
-          if (processItem?.parameters) {
-            for (const parameterKey of Object.keys(processItem.parameters)) {
-              const value = eval(processItem.parameters[parameterKey]);
 
-              parameters[parameterKey] = value;
+    const allProcessData = await ProcessDataModel.find({});
+    for (const processValue of allProcessData) {
+      parameters = {};
+      console.log(`Running: ${processValue.name}`);
+      for (const processItem of processValue.process) {
+        switch (processItem.name) {
+          case "generate-data": {
+            if (processItem?.parameters) {
+              for (const parameterKey of Object.keys(processItem.parameters)) {
+                const value = eval(processItem.parameters[parameterKey]);
+
+                parameters[parameterKey] = value;
+              }
             }
+            break;
           }
-          break;
-        }
-        case "api": {
-          let updatedCurl = processItem.curl;
-          Object.keys(parameters).forEach((key) => {
-            const regex = new RegExp(`{parameters\\['${key}']}`, "g");
-            updatedCurl = updatedCurl.replace(regex, parameters[key]);
-          });
-          // Add uuid
-          while (updatedCurl.includes("{uuid}")) {
-            updatedCurl = updatedCurl.replace("{uuid}", uuidv4());
-          }
+          case "api": {
+            let updatedCurl = processItem.curl;
+            Object.keys(parameters).forEach((key) => {
+              const regex = new RegExp(`{parameters\\['${key}']}`, "g");
+              updatedCurl = updatedCurl.replace(regex, parameters[key]);
+            });
+            // Add uuid
+            while (updatedCurl.includes("{uuid}")) {
+              updatedCurl = updatedCurl.replace("{uuid}", uuidv4());
+            }
 
-          const requestOptions = parseCurlString(updatedCurl);
+            const requestOptions = parseCurlString(updatedCurl);
 
-          index++;
+            index++;
 
-          const result = await performRequest(requestOptions);
+            const result = await performRequest(requestOptions);
 
-          if (processItem?.parameters) {
-            for (const parameterKey of Object.keys(processItem.parameters)) {
-              if (!processItem.parameters[parameterKey]) {
-                parameters[parameterKey] = result;
-              } else {
-                if (processItem.parameters[parameterKey][0] != "#") {
-                  parameters[parameterKey] = get(
-                    result,
-                    processItem.parameters[parameterKey]
-                  );
+            if (processItem?.parameters) {
+              for (const parameterKey of Object.keys(processItem.parameters)) {
+                if (!processItem.parameters[parameterKey]) {
+                  parameters[parameterKey] = result;
                 } else {
-                  const listKey =
-                    processItem.parameters[parameterKey].split("#");
-                  let tmp = get(result, listKey[1]);
+                  if (processItem.parameters[parameterKey][0] != "#") {
+                    parameters[parameterKey] = get(
+                      result,
+                      processItem.parameters[parameterKey]
+                    );
+                  } else {
+                    const listKey =
+                      processItem.parameters[parameterKey].split("#");
+                    let tmp = get(result, listKey[1]);
 
-                  const command = listKey[2].replace("{tmp}", tmp);
-                  const value = eval(command);
+                    const command = listKey[2].replace("{tmp}", tmp);
+                    const value = eval(command);
 
-                  parameters[parameterKey] = value;
+                    parameters[parameterKey] = value;
+                  }
                 }
               }
             }
+            break;
           }
-          break;
-        }
-        case "postgres": {
-          const client = new Client({
-            host: processItem.config.host,
-            database: processItem.config.db,
-            user: processItem.config.username,
-            password: processItem.config.password,
-            port: processItem.config.port,
-          });
-          await client.connect();
+          case "postgres": {
+            const client = new Client({
+              host: processItem.config.host,
+              database: processItem.config.db,
+              user: processItem.config.username,
+              password: processItem.config.password,
+              port: processItem.config.port,
+            });
+            await client.connect();
 
-          let query = processItem.query;
-          Object.keys(parameters).forEach((key) => {
-            const regex = new RegExp(`{parameters\\['${key}']}`, "g");
-            query = query.replace(regex, parameters[key]);
-          });
-          const result = (await client.query(query)).rows[0];
-          await client.end();
+            let query = processItem.query;
+            Object.keys(parameters).forEach((key) => {
+              const regex = new RegExp(`{parameters\\['${key}']}`, "g");
+              query = query.replace(regex, parameters[key]);
+            });
+            const result = (await client.query(query)).rows[0];
+            await client.end();
 
-          if (processItem?.parameters) {
-            for (const parameterKey of Object.keys(processItem.parameters)) {
-              if (!processItem.parameters[parameterKey]) {
-                parameters[parameterKey] = result;
-              } else {
-                if (processItem.parameters[parameterKey][0] != "#") {
-                  parameters[parameterKey] = get(
-                    result,
-                    processItem.parameters[parameterKey]
-                  );
+            if (processItem?.parameters) {
+              for (const parameterKey of Object.keys(processItem.parameters)) {
+                if (!processItem.parameters[parameterKey]) {
+                  parameters[parameterKey] = result;
                 } else {
-                  const listKey =
-                    processItem.parameters[parameterKey].split("#");
-                  let tmp = get(result, listKey[1]);
+                  if (processItem.parameters[parameterKey][0] != "#") {
+                    parameters[parameterKey] = get(
+                      result,
+                      processItem.parameters[parameterKey]
+                    );
+                  } else {
+                    const listKey =
+                      processItem.parameters[parameterKey].split("#");
+                    let tmp = get(result, listKey[1]);
 
-                  const command = listKey[2].replace("{tmp}", tmp);
-                  const value = eval(command);
+                    const command = listKey[2].replace("{tmp}", tmp);
+                    const value = eval(command);
 
-                  parameters[parameterKey] = value;
+                    parameters[parameterKey] = value;
+                  }
                 }
               }
             }
+            break;
           }
-          break;
-        }
-        case "delay": {
-          await delayWithAsync(Number(processItem.timeMs));
-          break;
+          case "delay": {
+            await delayWithAsync(Number(processItem.timeMs));
+            break;
+          }
         }
       }
+      console.log(parameters, "parameters");
     }
-    console.log(parameters, "parameters");
   } catch (error) {}
 }
 
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -29,7 +29,7 @@ async function performRequest(requestOptions) {
 
     return response.data;
   } catch (error) {
-    //console.error("Error:", error);
+    console.error("Error:", error);
     console.error("Error:", error?.response?.data);
     throw error;
   }
