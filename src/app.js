eba968a9a549227628ca3ccd086669217c786fb8
Dong Tran <thiendong.iuh@gmail.com>
2024-03-24 23:33:44 +0700

    add log

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ const {
 const { parse, stringify } = require("flatted");
 
 async function startApp() {
+  console.log(process.env.MONGO_URI, "process.env.MONGO_URI");
   const connection = await connectToMongo(process.env.MONGO_URI);
 
   app.listen(process.env.PORT, () => {
